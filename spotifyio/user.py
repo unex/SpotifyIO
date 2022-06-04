0d05101bf15c391ba327c9c214a318592b25102d
Unexceptional <git@unexceptional.net>
2022-06-03 20:14:06 -0600

    optimize User

--- spotifyio/user.py
+++ spotifyio/user.py
@@ -6,17 +6,26 @@ from .mixins import Url
 
 
 class User(Url):
+    __slots__ = (
+        "id",
+        "uri",
+        "external_urls",
+        "display_name",
+        "followers",
+        "images",
+    )
+
     def __init__(self, http: HTTPClient, data: dict) -> None:
         self._http = http
 
-        self.display_name: str = data.pop("display_name")
-        self.external_urls: dict = data.pop("external_urls")
-        self.followers: int = data.pop("followers").get("total")
-        self.url: str = data.pop("href")
-        self.id: str = data.pop("id")
-        self.images: List[Asset] = [Asset(**a) for a in data.pop("images")]
-        self.uri: str = data.pop("uri")
+        self.id: str = data["id"]
+        self.uri: str = data["uri"]
+        self.external_urls: dict = data["external_urls"]
+        self.display_name: str = data["display_name"]
+        self.followers: int = data["followers"]["total"]
+        self.images: List[Asset] = [Asset(**a) for a in data["images"]]
 
 
 class ClientUser(User):
-    pass
+    __slots__ = ()
+
