e5778bc2f38a64b621c5f5b1d1cd4ff1006ae710
Unexceptional <git@unexceptional.net>
2022-06-03 21:08:34 -0600

    add forgotten imports

--- spotifyio/__init__.py
+++ spotifyio/__init__.py
@@ -1,9 +1,12 @@
 __version__ = "0.0.1a"
 
 
+from .album import *
+from .artist import *
 from .asset import *
 from .auth import *
 from .client import *
+from .exceptions import *
 from .scopes import *
 from .track import *
 from .user import *
