f838ca028915da17f14f9d0f81040c00c38d5b0e
Unexceptional <git@unexceptional.net>
2022-06-03 20:18:17 -0600

    User - add scoped vars

--- spotifyio/user.py
+++ spotifyio/user.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from .http import HTTPClient
 from .asset import Asset
@@ -13,6 +13,10 @@ class User(Url):
         "display_name",
         "followers",
         "images",
+        "email",
+        "country",
+        "product",
+        "explicit_content",
     )
 
     def __init__(self, http: HTTPClient, data: dict) -> None:
@@ -24,6 +28,13 @@ class User(Url):
         self.display_name: str = data["display_name"]
         self.followers: int = data["followers"]["total"]
         self.images: List[Asset] = [Asset(**a) for a in data["images"]]
+
+        self.email: Optional[str] = data.get("email")  # user-read-email
+        self.country: Optional[str] = data.get("country")  # user-read-private
+        self.product: Optional[str] = data.get("product")  # user-read-private
+        self.explicit_content: Optional[dict] = data.get(
+            "explicit_content"
+        )  # user-read-private
 
 
 class ClientUser(User):
