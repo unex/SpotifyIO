83237c25751b3643d478d9325e7839d0ad469e80
Unexceptional <git@unexceptional.net>
2022-06-04 18:56:56 -0600

    fix lazy Paginator extra request

--- spotifyio/utils/paginator.py
+++ spotifyio/utils/paginator.py
@@ -8,13 +8,14 @@ class Paginator:
         self._func = func
 
         self._limit: int = kwargs.pop("limit", None)
+        self._total: int = float('inf')
 
         self.count: int = 0
         self.data: list = []
 
         if _data := kwargs.pop("_data"):
             self.data = _data["items"]
-            self.count = len(self.data)
+            self._total = _data["total"]
 
         self._args = args
         self._kwargs = kwargs
@@ -38,16 +39,21 @@ class Paginator:
 
         req = await self._func(*self._args, **kwargs)
 
+        self._total = req.pop("total")
+
         self.data = req.pop("items")
 
     async def __anext__(self):
         if self._limit is not None and self.count == self._limit:
             raise StopAsyncIteration
 
+        if self._total == self.count:
+            raise StopAsyncIteration
+
         if not self.data:
             await self._make_req()
 
-        # wastes an extra request but I am lazy
+        # this should never happen
         if not self.data:
             raise StopAsyncIteration
 
